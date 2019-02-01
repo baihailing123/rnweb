2aba420dfd6d770f54e356ae32196d4e4e3cfd03
baihailing <baihailing@qiyi.con>
2019-02-01 14:22:10 +0800

    second commit

--- rnweb/babel.config.js
+++ rnweb/babel.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   presets: ["module:metro-react-native-babel-preset","@babel/preset-env"],
-  plugins: ["@babel/plugin-transform-runtime",["transform-class-properties"]]
+  plugins: ["@babel/plugin-transform-runtime",["@babel/plugin-proposal-class-properties"]]
 }
